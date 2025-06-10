43839bd7b49e2165a1b4cb405c3f4d218284676f
KarolGuerrero <stefaniortiz2905@gmail.com>
2025-06-10 05:00:35 +0000

    test htmlprocessor

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -2,22 +2,27 @@ import boto3
 from botocore.exceptions import ClientError
 
 CRAWLER_NAME = "noticias_crawler"
+
+
 def handler(event=None, context=None):
     glue = boto3.client("glue")
     try:
         glue.start_crawler(Name=CRAWLER_NAME)
         return {
             "status": "ok",
-            "mensaje": f"Crawler '{CRAWLER_NAME}' ejecutado correctamente",
+            "mensaje": (
+                f"Crawler '{CRAWLER_NAME}' ejecutado correctamente"
+            ),
         }
     except ClientError as e:
         if e.response['Error']['Code'] == 'CrawlerRunningException':
             return {
                 "status": "error",
-                "mensaje": f"El crawler '{CRAWLER_NAME}' ya está en ejecución.",
+                "mensaje": (
+                    f"El crawler '{CRAWLER_NAME}' ya está en ejecución."
+                ),
             }
-        else:
-            return {
-                "status": "error",
-                "mensaje": str(e),
-            }
+        return {
+            "status": "error",
+            "mensaje": str(e),
+        }
