c374bfbc3abe0de9c8e238d8703baa32068565b6
KarolGuerrero <stefaniortiz2905@gmail.com>
2025-06-10 03:43:55 +0000

    test scraper

--- scraper/app.py
+++ scraper/app.py
@@ -9,7 +9,7 @@ BUCKET = 'parcial-save-scrapper'
 def download_and_upload(url, source_name):
     response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
     if response.status_code == 200:
-        s3 = boto3.client('s3')  # 👈 mover aquí
+        s3 = boto3.client('s3')  
         today = datetime.now().strftime("%Y-%m-%d")
         filename = f"headlines/raw/{source_name}-{today}.html"
         s3.put_object(
@@ -22,13 +22,15 @@ def download_and_upload(url, source_name):
     return f"Failed to fetch {source_name}: {response.status_code}"
 
 
-
+#prueba
 def handler(event=None, context=None):
     """Handler principal que descarga HTML de varias fuentes."""
     logs = []
+    
     logs.append(
         download_and_upload("https://www.eltiempo.com/", "eltiempo")
     )
+    
     logs.append(
         download_and_upload("https://www.elespectador.com/", "elespectador")
     )
